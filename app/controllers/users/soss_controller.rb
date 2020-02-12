33630fa096084abda34c1c97da55693fd3aefa7e
enz <enzo_naguiat@yahoo.com>
2020-02-12 23:07:39 +0900

    create a login_helper that handles login/signup/logout navigation buttons. replace the login/signup buttons with the helper inside the views.

--- app/controllers/users/soss_controller.rb
+++ app/controllers/users/soss_controller.rb
@@ -1,4 +1,6 @@
 class Users::SossController < ApplicationController
+  before_action :authenticate_user!
+  
   def new
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def login_helper
+    if current_user
+      (tag.li(link_to "User Profile", "#")) + 
+      (tag.li(link_to "Logout", destroy_user_session_path, method: :delete))
+    else
+      (tag.li(link_to "Login", new_user_session_path)) +
+      (tag.li(link_to "Signup", new_user_registration_path))
+    end
+  end
 end
