9f8d09418c3e555f8a251c5952b47bbb7b1682d8
enz <enzo_naguiat@yahoo.com>
2020-02-15 15:37:46 +0900

    allow user to inspect and edit his/her contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,13 @@
 class ContactsController < ApplicationController
+  before_action :set_contact, only: [:show, :edit, :update, :destroy]
+  def show
+  end
+
   def index
-    @contacts = current_user.contacts
+    if current_user
+      @contacts = current_user.contacts
+    else
+      redirect_to user_session
   end
 
   def new
@@ -17,9 +24,23 @@ class ContactsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @contact.update(contact_params)
+      redirect_to contact_path(@contact.id)
+    else
+      render 'edit'
+    end
+  end
+
   private 
-  
     def contact_params
       params.require(:contact).permit(:name, :email)
+    end
+
+    def set_contact
+      @contact = Contact.find(params[:id])
     end
 end
