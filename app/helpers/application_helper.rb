0a3b5a532b05c8d3b6a8c911ebb74f23fd7eb7d2
enz <enzo_naguiat@yahoo.com>
2020-02-25 21:16:10 +0900

    update styling of navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def login_helper
     if current_user
-      (tag.li(link_to "User Profile", "#")) + 
-      (tag.li(link_to "Logout", destroy_user_session_path, method: :delete))
+      (link_to "User Profile", "#") + 
+      (link_to "Logout", destroy_user_session_path, method: :delete)
     else
-      (tag.li(link_to "Login", new_user_session_path)) +
-      (tag.li(link_to "Signup", new_user_registration_path))
+      (link_to "Login", new_user_session_path, class: "btn btn-primary") +
+      (link_to "Signup", new_user_registration_path, class: "btn btn-outline-primary")
     end
   end
 end
