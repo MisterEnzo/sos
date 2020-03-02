9e0fa754b6b00043dd76be577500c21e1580a5f6
enz <enzo_naguiat@yahoo.com>
2020-03-02 22:24:17 +0900

    add styling to logout and profile buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   def login_helper
     if current_user
-      (link_to "User Profile", "#") + 
-      (link_to "Logout", destroy_user_session_path, method: :delete)
+      (link_to "Profile", "#", class: "btn btn-outline-primary") + 
+      (link_to "Logout", destroy_user_session_path, method: :delete, class: "btn btn-outline-primary")
     else
       (link_to "Login", new_user_session_path, class: "btn btn-primary") +
       (link_to "Signup", new_user_registration_path, class: "btn btn-outline-primary")
