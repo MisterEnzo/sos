b156140f6eecdf66e2b694c989409b1e2226b9f8
enz <enzo_naguiat@yahoo.com>
2020-03-10 22:54:03 +0900

    link profile menu item to edit user registration path. style profile page. modify application layout to afix footer at the bottom. modify static page controller so that requests to the homepage will be redirected to the sos page if a user is logged-in

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,12 @@
 class StaticController < ApplicationController
+  before_action :send_to_sos
   def home
+  end
+
+  private
+
+  def send_to_sos
+    redirect_to sos_path if current_user
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def login_helper
     if current_user
-      (link_to "Profile", "#", class: "btn btn-outline-primary") + 
+      (link_to "Profile", edit_user_registration_path, class: "btn btn-outline-primary") + 
       (link_to "Logout", destroy_user_session_path, method: :delete, class: "btn btn-outline-primary")
     else
       (link_to "Login", new_user_session_path, class: "btn btn-primary")
