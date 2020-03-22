edc273b60f5be0889139a4131a4d7143ad7ed4dd
enz <enzo_naguiat@yahoo.com>
2020-03-22 17:27:40 +0900

    add spec for signing up a new user

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -5,6 +5,16 @@ feature 'Signing up' do
     visit('/')
     click_on('Sign-up')
     expect(page).to have_current_path(new_user_registration_path)
-    
+    fill_in 'user_name', with: 'sample'
+    fill_in 'user_email', with: 'sample@sos.com'
+    fill_in 'user_password', with: '123123'
+    fill_in 'user_password_confirmation', with: '123123'
+    click_button('Sign up')
+    expect(page).to have_content("Welcome! You have signed up successfully.")
+    expect(User.last.name).to eq('sample')
+    expect(page).to have_content("Contact")
+    expect(page).to have_content("Name")
+    expect(page).to have_content("Email")
+    expect(page).to have_button("Add Contact")
   end
 end
