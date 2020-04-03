cb95a8982bfb24790b5959c33ee17af41b0a0a44
enz <enzo_naguiat@yahoo.com>
2020-04-03 23:26:21 +0900

    refactor contacts factory. add index and refactor edit specs for contacts controller

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -1,6 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe ContactsController do
+
+  describe "GET index" do
+    context "user hasn't signed-in" do
+      it "redirects user to sign-in page" do
+        get :index
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+    context "user has signed-in" do
+      let(:user) { FactoryBot.create(:user) }
+      let(:contacts) { FactoryBot.create_list(:contact, 5) }
+      it "shows all the contacts of the user" do
+        sign_in(user)
+        get :index
+        # expect(assigns[:contacts]).to not_be_empty
+        # expect(assigns(:contacts)).to match_array(contacts)
+      end
+
+      it "renders :index template" do
+        sign_in(user)
+        get :index
+        expect(response).to render_template(:index)
+      end
+    end
+  end
+
   describe "GET new" do
     let(:user) { FactoryBot.create(:user) }
 
@@ -28,18 +55,31 @@ RSpec.describe ContactsController do
         expect(response).to redirect_to(contacts_path)
       end
 
-      it "creates new contact in the database" do
-        sign_in(user)
-        expect {
-          post :create, params: { contact: FactoryBot.attributes_for(:contact) }
-        }.to change(Contact, :count).by(1)
+      context "enters valid data" do
+        it "creates new contact in the database" do
+          sign_in(user)
+          expect {
+            post :create, params: { contact: FactoryBot.attributes_for(:contact) }
+          }.to change(Contact, :count).by(1)
+          expect(response).to redirect_to(contacts_path) 
+        end
+      end
+      
+      context "enters invalid data" do
+        let(:invalid_contact) { FactoryBot.attributes_for(:contact, name: '', email: '') }
+        it "redirects to contact#new if the data is invalid" do
+          sign_in(user)
+          expect {
+            post :create, params: { contact: invalid_contact }
+          }.to change(Contact, :count).by(0)
+        end
       end
     end
   end
 
   describe "GET edit" do
     let(:user) { FactoryBot.create(:user) }
-    let(:contact) { FactoryBot.create(:contact, user: user)}
+    let(:contact) { FactoryBot.create(:contact)}
     
     context "User doesn't sign-in" do
       it "redirects user to sign-up when trying to edit" do
@@ -53,6 +93,12 @@ RSpec.describe ContactsController do
         sign_in(:user)
         get :edit, params: { id: 100 }
         expect(response).to redirect_to(contacts_path)
+      end
+
+      it "User is presented with the correct contact to edit" do
+        sign_in(:user)
+        get :edit, params: { id: contact.id }
+        expect(assigns(:contact)).to eq(contact)
       end
     end
   end
--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :contact do
+    user
     sequence(:name) { |n| "Contact_#{n}" }
-    sequence(:email) { |n| "contact_#{n}@example.com"}
+    sequence(:email) { |n| "contact_#{n}@example.com" }
   end
 end
